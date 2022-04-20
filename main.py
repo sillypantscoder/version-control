1118e3e9e23a31b5643037dfef23f1192e10a8ca
Jason Roman <roman.jason29@svvsd.org>
2022-04-20 14:21:08 -0600

    Added commit graphics

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
+import ver
 
 WHITE = (255, 255, 255)
 
 SCREENSIZE = [500, 500]
 screen = pygame.display.set_mode(SCREENSIZE)
+
+commits = ver.getCommits()
 
 c = pygame.time.Clock()
 running = True
@@ -11,6 +14,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    # Drawing
     screen.fill(WHITE)
+    for i in range(len(commits)):
+        r = 15
+        if pygame.Rect(0, i * 50, 50, 50).collidepoint(pygame.mouse.get_pos()): r = 20
+        pygame.draw.circle(screen, (0, 0, 0), (25, (i * 50) + 25), r)
+        for n in commits[i].getNextCommits():
+            pygame.draw.line(screen, (0, 0, 0), (25, (i * 50) + 25), (25, (n.index * 50) + 25), 5)
     pygame.display.flip()
     c.tick(60)
--- ver.py
+++ ver.py
@@ -1,12 +1,25 @@
 import json
 import os
 
-def applyCommit(n: int):
-	f = open("commits.json", "r")
-	commit = json.load(f)[n]
-	f.close()
-	os.system("rm test_dir/*")
-	for i in commit["files"]:
-		f = open("test_dir/" + i, "w")
-		f.write(commit["files"][i])
+class Commit:
+	def __init__(self, n: int):
+		self.index = n
+		f = open("commits.json", "r")
+		self.commit = json.load(f)[n]
 		f.close()
+		self.files = self.commit["files"]
+		self.name = self.commit["name"]
+	def getNextCommits(self):
+		return [Commit(i) for i in self.commit["next"]]
+	def apply(self):
+		os.system("rm test_dir/*")
+		for i in self.commit["files"]:
+			f = open("test_dir/" + i, "w")
+			f.write(self.commit["files"][i])
+			f.close()
+
+def getCommits() -> "list[Commit]":
+	f = open("commits.json", "r")
+	commits = json.load(f)
+	f.close()
+	return [Commit(i) for i in range(len(commits))]
