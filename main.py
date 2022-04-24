c7641eb3384cd94189132707b106db8a11c5ba8d
Jason Roman <funjasonroman@gmail.com>
2022-04-24 16:20:04 -0600

    Added working commits

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ SCREENSIZE = [500, 500]
 screen = pygame.display.set_mode(SCREENSIZE)
 FONT = pygame.font.SysFont(pygame.font.get_default_font(), 20)
 
-commits = ver.getCommits()
-
 c = pygame.time.Clock()
 running = True
 while running:
@@ -18,6 +16,8 @@ while running:
 		if event.type == pygame.QUIT:
 			running = False
 	# Drawing
+	ver.updateFiles()
+	commits = ver.getCommits()
 	screen.fill(WHITE)
 	for i in range(len(commits)):
 		r = 15
@@ -28,10 +28,12 @@ while running:
 			screen.blit(rendered, (50, ((i + 1) * 50) - ((rendered.get_height() / 2) + 25)))
 			if pygame.mouse.get_pressed()[0]:
 				commits[i].apply()
-		pygame.draw.circle(screen, (0, 0, 0), (25, (i * 50) + 25), r)
+		color = (0, 0, 0)
+		if commits[i].type == "working": color = (255, 160, 0)
+		pygame.draw.circle(screen, color, (25, (i * 50) + 25), r)
 		if commits[i].index == ver.getCurrentCommit().index:
 			pygame.draw.circle(screen, (0, 255, 0), (25, (i * 50) + 25), r, 2)
 		for n in commits[i].getNextCommits():
 			pygame.draw.line(screen, (0, 0, 0), (25, (i * 50) + 25), (25, (n.index * 50) + 25), 5)
 	pygame.display.flip()
-	c.tick(60)
+	c.tick(10)
--- ver.py
+++ ver.py
@@ -9,6 +9,7 @@ class Commit:
 		f.close()
 		self.files: dict[str, str] = self.commit["files"]
 		self.name: str = self.commit["name"]
+		self.type: str = self.commit["type"]
 	def getNextCommits(self) -> "list[Commit]":
 		return [Commit(i) for i in self.commit["next"]]
 	def apply(self):
@@ -24,6 +25,8 @@ class Commit:
 		f = open("commits.json", "w")
 		f.write(json.dumps(commits, indent=4).replace("    ", "\t"))
 		f.close()
+	def __repr__(self):
+		return f"<Commit \"{self.name}\">"
 
 def getCommits() -> "list[Commit]":
 	f = open("commits.json", "r")
@@ -36,3 +39,22 @@ def getCurrentCommit() -> Commit:
 	commits = json.load(f)
 	f.close()
 	return Commit(commits["current"])
+
+def getFiles() -> "dict[str, str]":
+	f = os.listdir("test_dir")
+	return {i: open("test_dir/" + i, "r").read() for i in f}
+
+def updateFiles() -> None:
+	f = open("commits.json", "r")
+	commits = json.load(f)
+	f.close()
+	if commits["commits"][commits["current"]]["files"] != getFiles(): 												# if the files have changed:
+		if commits["commits"][commits["current"]]["type"] == "working": 												# if the current commit is a working commit:
+			commits["commits"][commits["current"]]["files"] = getFiles() 													# update the files
+		else: 																											# otherwise:
+			commits["commits"].append({"name": "Local Changes", "files": getFiles(), "next": [], "type": "working"}) 		# add a new working commit
+			commits["commits"][commits["current"]]["next"].append(len(commits["commits"]) - 1) 								# add the new commit to the current commit's next commits
+			commits["current"] = len(commits["commits"]) - 1 																# and switch to the new commit
+	f = open("commits.json", "w")
+	f.write(json.dumps(commits, indent=4).replace("    ", "\t"))
+	f.close()
